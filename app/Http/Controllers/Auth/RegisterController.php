6abc62696c13b635db17cb3e1e2cc8353703ec9b
nabil-e <nabilelassal@gmail.com>
2019-04-05 09:49:31 +0200

    maj nabil-e

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -94,7 +94,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' => bcrypt($data['password']),
             'confirmation_token' => str_replace('/', '', bcrypt(str_random(20)))
         ]);
     }
